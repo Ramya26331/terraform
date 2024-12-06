982da5d5ddec66453b6e4838a4445be6857cf6e2
Ramya26331 <roja26331@gmail.com>
2024-12-06 15:52:31 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "one" {
-count           =  3
+count           =  10
 ami             = "ami-0453ec754f44f9a4a"
   instance_type = "t2.micro"
   tags = {
