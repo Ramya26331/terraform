63111b9087c8b99ec6e79e76d67aef790369ebcc
Ramya26331 <roja26331@gmail.com>
2025-02-01 19:44:15 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,13 +6,7 @@ resource "aws_instance" "one" {
 count = 4
 ami = "ami-03d0482bdae49b626"
 instance_type = "t2.medium"
-key_name = "jaikeypair"
-vpc_security_group_ids = ["sg-00c149e94e24198ce"]
 tags = {
-Name = var.instance_names[count.index]
+name = "ramya"
 }
-}
-
-variable "instance_names" {
-default = ["jenkins", "nexus", "tomcat-1", "tomcat-2"]
 }
