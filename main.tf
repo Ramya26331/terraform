dca8deb1e4ac47117d5bb633d78d3756d5420927
Ramya26331 <roja26331@gmail.com>
2025-02-03 10:41:21 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,13 @@
 provider "aws" {
-region = "us-east-1"
+  region = "us-east-1"
 }
 
 resource "aws_instance" "one" {
-count = 4
-ami = "ami-03d0482bdae49b626"
-instance_type = "t2.medium"
-tags = {
-name = "ramya"
-}
+  count         = 4
+  ami           = "ami-0866a3c8686eaeeba"
+  instance_type = "t2.medium"
+  tags = {
+    Name = "ramya"
+  }
 }
 
